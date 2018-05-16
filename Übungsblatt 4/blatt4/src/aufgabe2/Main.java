7303ed8f5cb186a0954935535411dd2d4c06a982
Patrick <pgreher@gmx.de>
2018-05-16 19:58:59 +0200

    Aufgabe 2 a und b

--- Übungsblatt 4/blatt4/src/aufgabe2/Main.java
+++ Übungsblatt 4/blatt4/src/aufgabe2/Main.java
@@ -1,5 +1,8 @@
 package aufgabe2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main 
 {
 	//Static variables to count the number of comparisons
@@ -84,12 +87,57 @@ public class Main
 	/**
 	 * Sorts an array with mergesort.
 	 * @param array - the array to be sorted.
+	 * @return 
 	 */
 	public static void mergeSort(double[] array) {
+		// TODO: Implementation goes here. 
 		comparisonsOfMergeSort = 0;
-		// TODO: Implementation goes here.    
+		mergeSort1(array);
 	}
 	
+	private static double[] mergeSort1(double[]array) {
+		if (array.length > 1) {
+			comparisonsOfMergeSort++;
+			double[] left = new double[array.length/2];
+			double[] right = new double[array.length-left.length];
+			System.arraycopy(array, 0, left, 0, left.length);
+			System.arraycopy(array, left.length, right, 0, right.length);
+			mergeSort1(left);
+			mergeSort1(right);
+			merge(left,right,array);
+			return array;
+		}else {
+			return array;
+		}
+	}
+	
+	private static void merge(double[]left, double[]right, double[] sortedList){
+		int indexSortedList = 0;
+		int indexLeft = 0;
+		int indexRight = 0;
+		while(indexLeft < left.length && indexRight<right.length) {
+			comparisonsOfMergeSort++;
+			if(left[indexLeft]<=right[indexRight]) {
+				comparisonsOfMergeSort++;
+				sortedList[indexSortedList] = left[indexLeft];
+				indexLeft++;
+			}
+			else {
+				sortedList[indexSortedList] = right[indexRight];
+				indexRight++;
+			}
+			indexSortedList++;
+		}
+		if(indexLeft<left.length) {
+			comparisonsOfMergeSort++;
+			System.arraycopy(left, indexLeft, sortedList, indexSortedList, left.length-indexLeft);
+		}
+
+		if(indexRight<right.length) {
+			comparisonsOfMergeSort++;
+			System.arraycopy(right, indexRight, sortedList, indexSortedList, right.length-indexRight);
+		}
+	}
 	// TODO: Put additional methods for mergeSort here.
 	
 	/* END OF MERGESORT */
@@ -101,9 +149,47 @@ public class Main
 	 */
 	public static void quickSort(double[] array) {
 		comparisonsOfQuickSort = 0;
-		// TODO: Implementation goes here. 
+		// TODO: Implementation goes here.
+		quickSort1(array, 0,array.length-1);
 	}
-	
+	private static void quickSort1(double[]array, int left, int right) {
+		int index = teile(array, left, right);
+		if (left < index -1) {
+			comparisonsOfQuickSort++;
+			quickSort1(array, left, index-1);
+		}
+		if(index < right) {
+			comparisonsOfQuickSort++;
+			quickSort1(array, index, right);
+		}
+	}
+	private static int teile(double[] array, int left, int right) {
+		int i = left;
+		int k = right;
+		double tmp;
+		double pivot = array[(left+right)/2];
+		while (i<=k) {
+			comparisonsOfQuickSort++;
+			while(array[i] < pivot) {
+				comparisonsOfQuickSort++;
+				i++;
+			}
+			while(array[k] > pivot) {
+				comparisonsOfQuickSort++;
+				k--;
+			}
+			if(i<=k) {
+				comparisonsOfQuickSort++;
+				tmp = array[i];
+				array[i] = array[k];
+				array[k] = tmp;
+				i++;
+				k--;
+			}
+		}
+		
+		return i;
+	}
 	// TODO: Put additional methods for quicksort here.
 	
 	/* END OF QUICKSORT */
